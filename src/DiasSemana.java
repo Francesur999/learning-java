83712548e1dc8408322fe0270e6d43dd7d05d131
Francesur999 <francisco.s375282@cesurformacion.com>
2023-11-09 12:17:00 +0100

    Meses

--- src/DiasSemana.java
+++ src/DiasSemana.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class DiasSemana {
 
@@ -6,60 +7,74 @@ public class DiasSemana {
 		int mes;
 		int semana;
 		int diaSemana;
-
-		diaSemana = 1;
+		int numeroDia;
+		int mesCompleto;
+		
+		diaSemana = 0;
 
 		semana = 0;
 
-		mes = 1;
+		mes = 0;
+		
+		mesCompleto = 0;
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		do {
+		   System.out.print("Escribe un  dias entre 1 y 336: ");
+		   numeroDia = teclado.nextInt();
+		}while(numeroDia<1 || numeroDia>336);
+		
+		 
+		mesCompleto = numeroDia/28;
 
-		while (mes < 13) {
+		while (mes < mesCompleto ) {
 
-			if (mes == 1) {
+			if (mes == 0) {
 
 				System.out.print("Enero");
 
-			} else if (mes == 2) {
+			} else if (mes == 1) {
 
 				System.out.print("Febrero");
 
-			} else if (mes == 3) {
+			} else if (mes == 2) {
 
 				System.out.print("Marzo");
 
-			} else if (mes == 4) {
+			} else if (mes == 3) {
 
 				System.out.print("Abril");
 
-			} else if (mes == 5) {
+			} else if (mes == 4) {
 
 				System.out.print("Mayo");
 
-			} else if (mes == 6) {
+			} else if (mes == 5) {
 
 				System.out.print("Junio");
 
-			} else if (mes == 7) {
+			} else if (mes == 6) {
 
 				System.out.print("Julio");
 
-			} else if (mes == 8) {
+			} else if (mes == 7) {
 
 				System.out.print("Agosto");
 
-			} else if (mes == 9) {
+			} else if (mes == 8) {
 
 				System.out.print("Septiembre");
 
-			} else if (mes == 10) {
+			} else if (mes == 9) {
 
 				System.out.print("Octubre");
 
-			} else if (mes == 11) {
+			} else if (mes == 10) {
 
 				System.out.print("Noviembre");
 
-			} else if (mes == 12) {
+			} else if (mes == 11) {
 
 				System.out.print("Diciembre");
 
