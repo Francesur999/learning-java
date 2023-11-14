aba09bf4bfb1f6bbb28105e48b01051d80a80860
Francesur999 <francisco.s375282@cesurformacion.com>
2023-11-14 11:55:56 +0100

    Nuevos programillas

---
+++ src/Contador_Inverso.java
@@ -0,0 +1,34 @@
+
+public class Contador_Inverso {
+
+	public static void main(String[] args) {
+
+		int i = 1200;
+		
+
+		while (i <= 1200 && i >= 620) {
+			System.out.print(i+ " ");
+			i = i -20;
+
+		}
+		
+		System.out.println(" ");
+		
+		i = 1200;
+		
+		do {
+			System.out.print(i+ " ");
+			i = i -20;
+			
+		}while(i <= 1200 && i >= 620);
+		
+		System.out.println(" ");
+		
+		for(int j = 1200;j <= 1200 && j >= 620; j = j - 20 )
+		{
+		
+			System.out.print(j+ " ");
+
+	}
+}
+}
---
+++ src/DesafiaLogica.java
@@ -0,0 +1,16 @@
+
+public class DesafiaLogica {
+
+	public static void main(String[] args) {
+		int i;
+		i = 10;
+		
+	while(i>0 && i<11)	 {
+		System.out.println(i);
+		 i--;
+	}
+		
+
+	}
+
+}
