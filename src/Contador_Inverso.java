de36e2906e623cdda70f3e5f5b7491b2458d9894
Francesur999 <francisco.s375282@cesurformacion.com>
2023-11-14 13:01:07 +0100

    proximamente...

--- src/Contador_Inverso.java
+++ src/Contador_Inverso.java
@@ -1,34 +1,47 @@
 
+import java.util.Scanner;
+
 public class Contador_Inverso {
 
 	public static void main(String[] args) {
 
-		int i = 1200;
-		
+		int n1;
+		int n2;
+		int salt;
+		int cont2 = 0;
 
-		while (i <= 1200 && i >= 620) {
-			System.out.print(i+ " ");
-			i = i -20;
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.print("Escribe el numero maximo");
+		n1 = teclado.nextInt();
+
+		do {
+
+			System.out.print("Escribe el numero mínimo");
+			n2 = teclado.nextInt();
+
+		} while (n2 >= n1);
+
+		System.out.print("Escribe en cuanto en cuanto quieres saltar ");
+		salt = teclado.nextInt();
+		System.out.println(" ");
+
+		while (n1 >= n2) {
+			System.out.print(n1 + " ");
+			n1 = n1 - salt;
+
+			for (int cont = 0; cont == 10000; cont++) {
+
+				if (cont2 == 9) {
+					System.out.println();
+					cont2 = cont2 + 1;
+
+				}
+				cont2 = 0;
+
+			}
 
 		}
-		
-		System.out.println(" ");
-		
-		i = 1200;
-		
-		do {
-			System.out.print(i+ " ");
-			i = i -20;
-			
-		}while(i <= 1200 && i >= 620);
-		
-		System.out.println(" ");
-		
-		for(int j = 1200;j <= 1200 && j >= 620; j = j - 20 )
-		{
-		
-			System.out.print(j+ " ");
-
 	}
-}
+
 }
