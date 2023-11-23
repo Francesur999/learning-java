14fd0c1454bb137b30a4eeab8d8565247c70e019
Francesur999 <francisco.s375282@cesurformacion.com>
2023-11-23 10:13:21 +0100

    proximamnete ordenar

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,16 +1,47 @@
+import java.util.Random;
 
 public class Arrays {
 
 	public static void main(String[] args) {
-        
+
+		Random aleatorio;
+		aleatorio = new Random();
+		int[] listaPares;
 		int[] listaInteger = new int[10];
-        
-		
-		for (int i = 0; i < 10; i++) {
-            listaInteger[i] = i;
-			System.out.print(listaInteger[i]+" ");
+		int[] listaImpares;
+		int numPares = 0;
+		int numImpar = 0;
+		int contadorP;
+		int contadorIm;
+
+		for (int i = 0; i < listaInteger.length; i++) {
+			listaInteger[i] = aleatorio.nextInt();
+
 		}
 
-	}
+		for (int i = 0; i < listaInteger.length; i++) {
+			if ((listaInteger[i] % 2) == 0) {
 
-}
+				numPares = numPares + 1;
+
+			} else {
+				numImpar = numImpar + 1;
+			}
+		}
+		System.out.println(numPares);
+		System.out.println(numImpar);
+		listaPares = new int[numPares];
+		contadorP = 0;
+		listaImpares = new int[numImpar];
+        contadorIm = 0;
+		for (int i = 0; i < listaInteger.length; i++) {
+			if ((listaInteger[i] % 2) == 0) {
+				listaPares[contadorP] = listaInteger[i];
+                contadorP++;
+			}else {
+				
+			listaImpares[contadorIm] = listaInteger[i];
+			contadorIm++;
+		}
+	}
+}}
