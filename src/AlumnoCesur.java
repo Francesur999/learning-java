3d2d064e73b6fc3fd629c208d612c0069b523ce2
Francesur999 <francisco.s375282@cesurformacion.com>
2023-12-05 12:32:21 +0100

    chao chao chao

--- src/AlumnoCesur.java
+++ src/AlumnoCesur.java
@@ -1,62 +1,76 @@
 
 public class AlumnoCesur {
 
-	
-
 	public static void main(String[] args) {
-		
+
 		Alumno Fran = new Alumno("Fran", 18, "DAM");
-		System.out.println(Fran.toString());
-		
+
 		Alumno Andrés = new Alumno("Andrés", 20, "DAM");
-		System.out.println(Andrés.toString());
-		
+
 		Alumno Santiago = new Alumno("Santiago", 22, "DAM");
-		System.out.println(Santiago.toString());
-		
+
 		Alumno Ramon = new Alumno("Ramon", 24, "DAM");
-		System.out.println(Ramon.toString());
-		
+
 		Alumno Janhin = new Alumno("Janhin", 27, "DAM");
-		System.out.println(Janhin.toString());
-		
+
 		Alumno Alvaro = new Alumno("Alvaro", 18, "DAM");
-		System.out.println(Alvaro.toString());
-		
+
 		Alumno Alejandro = new Alumno("Alejandro", 23, "DAM");
-		System.out.println(Alejandro.toString());
-		
+
 		Alumno Adrian = new Alumno("Adrián", 25, "DAM");
-		System.out.println(Adrian.toString());
-		
+
 		Alumno Sinram = new Alumno("Fran", 23, "DAM");
-		System.out.println(Sinram.toString());
-		
+
 		Alumno JuanCarlos = new Alumno("Juan Carlos", 24, "DAM");
-		System.out.println(JuanCarlos.toString());
-		
+
 		Alumno Dario = new Alumno("Dario", 17, "DAM");
-		System.out.println(Dario.toString());
-		
+
 		Alumno JoseRamon = new Alumno("Jose Ramón", 39, "DAM");
-		System.out.println(JoseRamon.toString());
+
+		Alumno Juanan = new Alumno("Juanan", 26, "DAW");
+
+		Clase premiumPlus = new Clase("Premium plus");
+
+		premiumPlus.add(JoseRamon);
+		premiumPlus.add(Andrés);
+		premiumPlus.add(Fran);
+		premiumPlus.add(Santiago);
+		premiumPlus.add(Ramon);
+		premiumPlus.add(Dario);
+		premiumPlus.add(JuanCarlos);
+		premiumPlus.add(Alvaro);
+		premiumPlus.add(Janhin);
+		premiumPlus.add(Adrian);
+		premiumPlus.add(Alejandro);
+		premiumPlus.add(Sinram);
+		premiumPlus.add(Juanan);
+
+		for (int i = 0; i < premiumPlus.getNumAlumnos(); i++) {
+			System.out.println(premiumPlus.getAlumnos()[i]);
+
+			if (premiumPlus.getAlumnos()[i].getNombre() == "Dario") {
+				premiumPlus.getAlumnos()[i].setEdad(18);
+				System.out.println(premiumPlus.getAlumnos()[i]);
+			}
+
+			System.out.println(" ");
+
+		}
 		
-		Clase PremiumPlus = new Clase("Premium plus");
-		System.out.println(PremiumPlus.toString());
-		
-		PremiumPlus.add(JoseRamon);
-		PremiumPlus.add(Andrés);
-		PremiumPlus.add(Fran);
-		PremiumPlus.add(Santiago);
-		PremiumPlus.add(Ramon);
-		PremiumPlus.add(Dario);
-		PremiumPlus.add(JuanCarlos);
-		PremiumPlus.add(Alvaro);
-		PremiumPlus.add(Janhin);
-		PremiumPlus.add(Adrian);
-		PremiumPlus.add(Alejandro);
-		PremiumPlus.add(Sinram);
-		
+
+		for (int x = 0; x < premiumPlus.getNumAlumnos(); x++) {
+
+			if (premiumPlus.getAlumnos()[x].getCurso() == "DAW") {
+				System.out.print ("Este es alumno de DAW" );
+				System.out.println(premiumPlus.getAlumnos()[x]);
+
+			} else {
+				
+				System.out.print("Estos son alumnos de DAM");
+				System.out.println(premiumPlus.getAlumnos()[x]);
+			}
+		}
+
 	}
 
 }
@@ -73,7 +87,6 @@ class Alumno {
 		this.curso = curso;
 	}
 
-
 	public String getNombre() {
 		return nombre;
 	}
@@ -81,8 +94,6 @@ class Alumno {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
-	
 
 	public int getEdad() {
 		return edad;
@@ -92,8 +103,6 @@ class Alumno {
 		this.edad = edad;
 	}
 
-	
-
 	public String getCurso() {
 		return curso;
 	}
@@ -101,64 +110,62 @@ class Alumno {
 	public void setCurso(String curso) {
 		this.curso = curso;
 	}
-	
+
 	public String toString() {
-		
-		return "[Alumno: "+nombre+", Edad: "+edad+", Curso: "+curso+ "]";
+
+		return "[Alumno: " + nombre + ", Edad: " + edad + ", Curso: " + curso + "]";
 	}
 
 }
 
-class Clase{
-	
+class Clase {
+
 	public Clase(String nombre) {
-		 alumnos = new Alumno[TOTALALUMNOS];
-		 numAlumnos = 0;
-		 this.nombre = nombre;
+		alumnos = new Alumno[TOTALALUMNOS];
+		numAlumnos = 0;
+		this.nombre = nombre;
 	}
-	
+
 	private String nombre;
 	private int numAlumnos;
 	private Alumno[] alumnos;
 	private static final int TOTALALUMNOS = 15;
-	
-	
-	
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
-	
-	
 	public int getNumAlumnos() {
 		return numAlumnos;
 	}
+
 	public void setNumAlumnos(int numAlumnos) {
 		this.numAlumnos = numAlumnos;
 	}
-	
+
 	public Alumno[] getAlumnos() {
 		return alumnos;
 	}
+
 	public void setAlumnos(Alumno[] alumnos) {
 		this.alumnos = alumnos;
-		
+
 	}
-	
-public String toString() {
-		
-		return "[Clase: "+nombre+", Numero de alumnos: "+numAlumnos+" ]";
+
+	public String toString() {
+
+		return "[Clase: " + nombre + ", Numero de alumnos: " + numAlumnos + " ]";
 	}
-	
-	
-	public void add (Alumno nuevoAlumno) {
-		
-		if(numAlumnos<TOTALALUMNOS) {
-		alumnos[numAlumnos]=nuevoAlumno;
-		numAlumnos++;
+
+	public void add(Alumno nuevoAlumno) {
+
+		if (numAlumnos < TOTALALUMNOS) {
+			alumnos[numAlumnos] = nuevoAlumno;
+			numAlumnos++;
 		}
 	}
 }
