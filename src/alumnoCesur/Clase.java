4b1d904af09dbcbe5c0f032ff3b8bac276f39866
Francesur999 <francisco.s375282@cesurformacion.com>
2023-12-11 10:28:12 +0100

    cambio estructuras

---
+++ src/alumnoCesur/Clase.java
@@ -0,0 +1,60 @@
+package alumnoCesur;
+
+class Clase {
+
+	public Clase(String nombre) {
+		alumnos = new Alumno[TOTALALUMNOS];
+		numAlumnos = 0;
+		this.nombre = nombre;
+	}
+
+	private String nombre;
+	private int numAlumnos;
+	private Alumno[] alumnos;
+	private static final int TOTALALUMNOS = 15;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getNumAlumnos() {
+		return numAlumnos;
+	}
+
+	public void setNumAlumnos(int numAlumnos) {
+		this.numAlumnos = numAlumnos;
+	}
+
+	public Alumno[] getAlumnos() {
+		return alumnos;
+	}
+
+	public void setAlumnos(Alumno[] alumnos) {
+		this.alumnos = alumnos;
+
+	}
+
+	public String toString() {
+
+		return "[Clase: " + nombre + ", Numero de alumnos: " + numAlumnos + " ]";
+	}
+
+	public void add(Alumno nuevoAlumno) {
+
+		if (numAlumnos < TOTALALUMNOS) {
+			alumnos[numAlumnos] = nuevoAlumno;
+			numAlumnos++;
+		}
+	}
+	
+	public void delete(String nombreAlumno) {
+		
+		
+		
+		
+	}
+}
