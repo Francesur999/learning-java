b041ac9d87cb0a037b49426f24d7fc567062ff25
Francesur999 <francisco.s375282@cesurformacion.com>
2023-12-13 09:42:56 +0100

    COSAS

--- src/alumnoCesur/Administrativo.java
+++ src/alumnoCesur/Administrativo.java
@@ -1,44 +1,27 @@
 package alumnoCesur;
 
-class Administrativo {
+class Administrativo extends Persona{
 	
 	
 	// Atributos
-
-	String nombre;
-	int edad;
-	boolean idiomas ;
+	
+	
+	private boolean idiomas ;
 	
 	//Constructor
 	
 	public Administrativo(String nombre, int edad, boolean idiomas) {
-		super();
-		this.nombre = nombre;
-		this.edad = edad;
+		super(nombre, edad);
 		this.idiomas = idiomas;
 	}
 	
 	public Administrativo(String nombre) {
-		super();
-		this.nombre = nombre;
+		
+		super(nombre, 0);
 		this.idiomas = false;
 	}
 	
 	//Getters y Setters
-	
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-	public int getEdad() {
-		return edad;
-	}
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
 	
 	public boolean isIdiomas() {
 		return idiomas;
--- src/alumnoCesur/CentroEstudio.java
+++ src/alumnoCesur/CentroEstudio.java
@@ -2,4 +2,31 @@ package alumnoCesur;
 
 public class CentroEstudio {
 
+	// ATRIBUTOS
+	private final int MAXCLASES;
+	private final int MAXADMIN;
+	private String nombre;
+	private Administrativo[] administrativos;
+	private Clase[] clases;
+
+	// CONSTRUCTOR
+
+	public CentroEstudio(int mAXCLASES, int mAXADMIN, String nombre) {
+		super();
+		MAXCLASES = mAXCLASES;
+		MAXADMIN = mAXADMIN;
+		this.nombre = nombre;
+		this.administrativos = new Administrativo[MAXADMIN];
+		this.clases= new Clase[MAXCLASES];
+	}
+
+	public CentroEstudio(String nombre) {
+		
+		this.nombre = nombre;
+		MAXADMIN = 10;
+		MAXCLASES = 10;
+		this.administrativos = new Administrativo[MAXADMIN];
+		this.clases= new Clase[MAXCLASES];
+	}
+
 }
